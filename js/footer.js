dc7312944c099679bcf883f83732ae993c8a4add
aaronrs2002 <aaronrs2002@yahoo.com>
2022-07-21 09:52:34 -0700

    Update footer.js
    added links obj

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,24 @@
+//START SOCIAL MEDIA
 const socialMedia = [
     { link: "https://www.linkedin.com/in/aaronrs2002", theClass: "fab fa-linkedin" },
     { link: "https://github.com/aaronrs2002", theClass: "fab fa-github" },
     { link: "https://www.youtube.com/channel/UC_cqZ5WhobgOTFjRqcZKgKg", theClass: "fab fa-youtube" },
-    { link: "mailto:aaron@web-presence.biz", theClass: "far fa-paper-plane" },
+    { link: "https://www.instagram.com/aaronrs2002/", theClass: "fab fa-instagram" },
+    { link: "mailto:aaron@web-presence.biz", theClass: "far fa-paper-plane" }
+
 ];
 let socialHTML = "";
 for (let i = 0; i < socialMedia.length; i++) {
     socialHTML = socialHTML + "<a class='p-2' href='" + socialMedia[i].link + "' target='_blank' title='" + socialMedia[i].link + "' ><i class='" + socialMedia[i].theClass + "' ></i></a>";
 }
 document.querySelector("#socialList").innerHTML = socialHTML;
+
+//START GAMES
+const gameLinks = [{ link: "https://mechanized-aesthetics.net/TEST/black-jack/index.html", game: "Black Jack" },
+{ link: "https://mechanized-aesthetics.net/TEST/dice/index.html", game: "Craps" }
+];
+let gameHTML = "";
+for (let i = 0; i < gameLinks.length; i++) {
+    gameHTML = gameHTML + "<a href='" + gameLinks[i].link + "' class='btn btn-secondary'>" + gameLinks[i].game + "</a>";
+}
+document.querySelector("#gameLinks").innerHTML = gameHTML;

aa6a2a5538f555f1247d445b9262c8729dcac7c9
Aaron Smith <aaron@web-presence.biz>
2022-08-03 08:23:02 -0700

    fix footerobj

--- js/footer.js
+++ js/footer.js
@@ -16,7 +16,7 @@ document.querySelector("#socialList").innerHTML = socialHTML;
 //START GAMES
 const gameLinks = [{ link: "https://aaronrs2002.github.io/black-jack/", game: "Black Jack" },
 { link: "https://aaronrs2002.github.io/craps/", game: "Craps" },
-{link:"https://aaronrs2002.github.io/javascript-slot-machine/index.html", game:"Slots"];
+{ link: "https://aaronrs2002.github.io/javascript-slot-machine/index.html", game: "Slots" }];
 let gameHTML = "";
 for (let i = 0; i < gameLinks.length; i++) {
     gameHTML = gameHTML + "<a href='" + gameLinks[i].link + "' class='btn btn-secondary'>" + gameLinks[i].game + "</a>";

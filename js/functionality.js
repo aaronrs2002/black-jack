97eae7062f978dc254f05057dde2181c8160b5b5
Aaron Smith <aaron@web-presence.biz>
2022-08-08 10:21:10 -0700

    if 21 for deal the card is now shown3

--- js/functionality.js
+++ js/functionality.js
@@ -132,9 +132,8 @@ function removeCards(dealerCards, playerCards) {
 function ckInsurance(card1, card2) {
     setPlayerMoney(playerMoney - 5);
     if ((card1 + card2) === 21) {
-
-        document.querySelector(".hiddenDealerCard").classList.add(dealerCards[0].title);
-        document.querySelector(".hiddenDealerCard").classList.remove(".hiddenDealerCard");
+        document.querySelector("[data-dealer='0']").classList.remove("hiddenDealerCard");
+        document.querySelector("[data-dealer='0']").classList.add(dealerCards[0].title);
         document.getElementById("dealerTotal").innerHTML = "DEALER HAS 21. Good job insuring.";
         showAlert("lose", "DEALER HAS 21! ", "alert-danger");
     } else {

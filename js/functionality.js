283cd8c964b9a7e6e64b014302fc63cb7b4c07eb
Aaron Smith <aaron@web-presence.biz>
2026-08-22 08:22:51 -0700

    5 insurance bug fix5

--- js/functionality.js
+++ js/functionality.js
@@ -133,7 +133,14 @@ function removeCards(dealerCards, playerCards) {
 }
 
 function ckInsurance(card1, card2) {
-    setPlayerMoney(playerMoney - 5, null, null);
+
+    if (localStorage.getItem("balance") && Number(localStorage.getItem("balance"))) {
+        playerMoney = Number(localStorage.getItem("balance"));
+    }
+
+
+    setPlayerMoney((Number(playerMoney) - 5), null, null);
+    console.log("playerMoney - 5: " + (Number(playerMoney) - 5));
     if ((card1 + card2) === 21) {
         document.querySelector("[data-dealer='0']").classList.remove("hiddenDealerCard");
         document.querySelector("[data-dealer='0']").classList.add(dealerCards[0].title);
